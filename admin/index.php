a23141118db3f19f2ce14c6210b383759bfa73c7
Meli Melinda <melinawada29@gmail.com>
2018-11-28 10:04:37 +0700

    ref1

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Form</title>
+<meta charset="utf-8">
+
+</head>
+<body>
+
+<form method="post" action="cek_login.php">
+    <input name="id_user"
+	type="text" id="id_user" placeholder="Masukan email Anda"><br>
+    <input name="password" type="password"
+		id="password" placeholder="Masukan Passord"><br>
+    <input type="submit" name="kirim" value="Kirim">
+</form>
+</body>
+</html>
